16fa924a4747e4422ee5c587d73ce6ed726d0aec
Nelu Platonov <platon.nelu@yahoo.com>
2020-10-31 23:38:24 +0100

    successfully serving index page

--- src/main/java/com/neluplatonov/eurder/api/controllers/IndexController.java
+++ src/main/java/com/neluplatonov/eurder/api/controllers/IndexController.java
@@ -1,15 +1,15 @@
 package com.neluplatonov.eurder.api.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String getIndex(){
-        return "Hello";
+        return "index";
     }
 }
